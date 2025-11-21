4668bb8e35bc592a7610623df8868e97162f2052
Benny-JH-L <133819720+Benny-JH-L@users.noreply.github.com>
2025-11-21 02:26:14 -0700

    task2 fixes
    - fixed a bug where if the hardcoded password shared the same char we are using to guess with (ie 'z') the program fails
    - and intern fixed another bug that arose, where we guess a password of 1 length longer than the hardcoded password
    - added few more test cases (some compiled with the secret2-my.c version!)

--- a4/a4secret/task2/guess2.cpp
+++ a4/a4secret/task2/guess2.cpp
@@ -18,6 +18,8 @@
 
 using namespace std;
 
+const int MAX_PASS_LENGTH = 64;
+
 // The guess<N>() function tries to guess a hardcoded secret from a compiled
 // executable, whos path is given as `exepath` parameter.
 // The input executable is assumed to behave as follows:
@@ -39,7 +41,8 @@ const char * guess2(const char * exepath)
     
     const int ASCII_z = 'z';
     int8_t exitCodeOf_strcmp = 1;
-    for (int index = 0; index < 1024 && exitCodeOf_strcmp != ASCII_z; index++)
+    // for (int index = 0; index <= MAX_PASS_LENGTH && exitCodeOf_strcmp != ASCII_z; index++)
+    for (int index = 0; index <= MAX_PASS_LENGTH; index++)
     {
         secret[index] = 'z';
 
@@ -63,6 +66,20 @@ const char * guess2(const char * exepath)
         {
             // success !!!
             DEBUG && printf("success\n");
+            return secret;
+        }
+
+        if (exitCodeOf_strcmp == exitCode)
+        {
+            // case where the hardcoded char is the char we are using to guess with
+            DEBUG && cout << "found char 'z' | strcmp() return = " << (int)exitCodeOf_strcmp << " | return code of '" << exepath << "' = " << exitCode << endl;
+            secret[index] = 'z'; // update the secret to the hardcoded char we found 
+            continue;
+        }
+        // found the terminating '\0' character, ie we tried a password of length 1 greater than the hardcoded password
+        else if (exitCodeOf_strcmp == -ASCII_z)
+        {
+            secret[index] = (char)NULL;   // remove the last character we added
             return secret;
         }
 
